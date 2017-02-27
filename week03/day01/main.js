c7d6175becbc752f456ccc9ecc119258b7e73f21
Mohammed Yafii <myafii@spartaglobal.co>
2017-02-27 15:50:40 +0000

    Add external JS file; more conditional statements

--- week03/day01/main.js
+++ week03/day01/main.js
@@ -4,14 +4,14 @@ var s = '';
 var b = true;
 var n = 1;
 var a = [];
-console.log('Using "typeof" operator:');
+console.log('--- Using "typeof" operator:');
 console.log(typeof o);
 console.log(typeof s);
 console.log(typeof b);
 console.log(typeof n);
 console.log(typeof a);
 
-console.log('Playing with different types:');
+console.log('--- Playing with different types:');
 var numberAsString = '5';
 var number1 = 3;
 var number2 = 7;
@@ -32,35 +32,43 @@ console.log(parseFloat('3.1415927'));
 console.log(parseFloat(3.56));
 console.log(parseFloat('blah'));
 
+console.log('--- Control flow:');
 var moneyInPocket = 20;
 
 if (moneyInPocket > 10) {
   console.log('Another drink please!');
 } else if (moneyInPocket > 5) {
-  console.log('Make mine a half then!');
+  console.log('Make mine a half then');
 } else {
-  console.log('Time to go home!');
+  console.log('Time to go home');
 }
 
+var countryOfOrigin = 'Kenya';
 var greeting = '';
-var countryOfOrigin = 'France';
 
 switch(countryOfOrigin) {
   case 'France':
     greeting = 'Bonjour';
     break;
   case 'Spain':
-    greeting = 'Hola!';
+    greeting = '¡Hola!';
     break;
   case 'Indonesia':
     greeting = 'Selamat sore';
     break;
   case 'Kenya':
-    greeting = 'Jambo';
+    greeting = 'Jambo!';
     break;
   default:
     greeting = 'Well hello there';
     break;
 }
-
 console.log(greeting);
+
+var dayOfWeek = 'Thursday';
+// logical AND
+if (dayOfWeek.toUpperCase() === 'THURSDAY' || dayOfWeek.toUpperCase() === 'FRIDAY') {
+  console.log('Pub thisarvo!');
+} else {
+  console.log('Bummer :-(');
+}
