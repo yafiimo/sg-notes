380c24bf1de03ee86bb9b2eb3b8be6f81e89b700
Mohammed Yafii <myafii@spartaglobal.co>
2017-03-02 14:16:40 +0000

    add jQuery

--- week03/day03-dom-events-jquery/main.js
+++ week03/day03-dom-events-jquery/main.js
@@ -1,53 +1,100 @@
 console.log('in main.js');
 
+var newCssClassName = 'minor';
+var homeIconElement = document.getElementById('home-button-icon');
 
-// Event listener for when the webpage loads
+// 1. Using setAttribute(), and maintaining existing class names
+var homeIconElementClassAttribute = homeIconElement.getAttribute('class');
+homeIconElementClassAttribute += ' ' + newCssClassName;
+homeIconElement.setAttribute('class', homeIconElementClassAttribute);
+
+// 2. Using className, and maintaining existing class names
+homeIconElement.className += ' ' + newCssClassName;
+
+// 3. Using classList.add()
+homeIconElement.classList.add(newCssClassName);
+
+
+$(function () {
+  console.log('DOM loaded (jQuery)');
+  console.log('--- using jQuery');
+
+  var listContainer1 = document.getElementById('item-list');
+  // It's useful to have a convention of naming the result of a jQuery call
+  // with a '$' prefix (as below).
+  // That way, it is immediately obvious that the variable is a jQuery object,
+  // and therefore has all the jQuery goodies on it.
+  // (But it's only a convention, not a rule).
+  var $listContainer2 = $('#item-list');
+  var $listItems = $('#item-list li');
+
+  console.log('listContainer1 (document.getElementById) :', listContainer1);
+  console.log('listContainer2 (jQuery):', $listContainer2);
+  console.log('listItems (jQuery):', $listItems);
+
+  var newListItem1 = $('<li>new item appended by jQuery</li>').addClass('garish');
+  var newListItem2 = $('<li></li>').addClass('dull').html('new item prepended by jQuery');
+  console.log('newListItem2.html():', newListItem2.html());
+  $listContainer2.append(newListItem1);
+  $listContainer2.prepend(newListItem2);
+
+  // Now let's add the 'emphasised' CSS class to all the <li> elements.
+
+  // Without jQuery, you'd have to do something like this:
+  // var listItems2 = document.getElementsByTagName('li');
+  // for (var i = 0 ; i < listItems2.length ; i++) {
+  //   listItems2[i].classList.add('emphasised');
+  // }
+
+  // And with jQuery, it's a lot more simple:
+  $listContainer2.children().addClass('emphasised');
+
+  // now to add a click handler to the button
+  $('#pickMeBtn').on('click', function() {
+    console.log('jQuery .on("click") handler');
+  });
+  // and this works too:
+  $('#pickMeBtn').click(function() {
+    console.log('jQuery .click() handler');
+  });
+});
 
 document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM loaded (with DOMContentLoaded event)');
+  console.log(document);
 
-  console.log(document); // shows the document
-
-
-// Add a new list item
-  var newListItem = document.createElement('li');
-  console.log('new list item:', newListItem);
-
-// add html to the new list item
-  newListItem.innerHTML = 'in New York';
-
-// add the class selected to newListItem
-  newListItem.classList.add('selected');
-
-
-// We are going to call .appendChild() on the parent <ul> element.
-// That ul element has an id attribute.
-
-// So we retrieve the DOM element that contains the list items,
-// because we want to call .appendChild() on it.
-// NOTE: we do not pass a CSS selector ('#item-list') when using
-// the getElementById() method.
-
-  var listContainer = document.getElementById('item-list');
-  console.log('this is the list container', listContainer);
-  listContainer.appendChild(newListItem);
-
-  var selectedListItems  = document.querySelectorAll('.selected');
+  console.log('--- using querySelectorAll:');
+  var selectedListItems = document.querySelectorAll('.selected');
 
   console.log('selectedListItems:', selectedListItems);
 
-  for (var i = 0; i < selectedListItems.length; i++) {
-    console.log('selected list item style', selectedListItems[i].style);
+  for(var i = 0 ; i < selectedListItems.length ; i++) {
+    console.log('selected list item style:', selectedListItems[i].style);
     selectedListItems[i].style.color = 'red';
   }
 
-  console.log(listContainer.children);
+  console.log('--- Create and append a new element');
+  var newListItem = document.createElement('li');
 
-// Adding click events
+  newListItem.innerHTML = 'in New York';
+  newListItem.setAttribute('class', 'muted');
+
+  // We are going to call .appendChild() on the parent <ul> element.
+  // That <ul> element has an id attribute.
+
+  // So we retrieve the DOM element that contains the list items,
+  // because we want to call .appendChild() on it.
+  // NOTE: we do NOT pass a CSS selector ('#item-list') to the
+  // .getElementById() method.
+  var listContainer = document.getElementById('item-list');
+
+  listContainer.appendChild(newListItem);
+
+  console.log('--- DOM events');
   var pickMeButton = document.getElementById('pickMeBtn');
-  console.log(pickMeButton);
 
-  pickMeButton.addEventListener('click', function() {
-    pickMeButton.innerHTML = 'CLICKED!';
+  pickMeButton.addEventListener('click', function () {
+    pickMeButton.innerHTML = 'CLICKED';
     alert('Picked!');
   });
 
