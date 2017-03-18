c9bf9dd4d327852d2a85e3c5dbb79a0220b08366
Mohammed Yafii <myafii@spartaglobal.co>
2017-03-18 07:22:29 +0000

    add test to delete an existing user

--- week05/day05-node/express-web-server-mvc-testing/test/users.spec.js
+++ week05/day05-node/express-web-server-mvc-testing/test/users.spec.js
@@ -9,27 +9,41 @@ var request;
 chai.should();
 chai.use(chaiHttp);
 
-describe('Users', function () {
+describe('Users', function() {
   beforeEach(function () {
     request = chai.request(app);
   });
+});
+
+describe('GET', function() {
 
   it('should return error for invalid URL GET', function (done) {
     request
-      .get('/invalid_url')
-      .end(function (err) {
-        expect(err).not.to.be.null;
-        done();
-      });
+        .get('/invalid_url')
+        .end(function (err) {
+          expect(err).not.to.be.null;
+          done();
+        });
   });
   it('should list all users for GET /users', function (done) {
     request
-      .get('/users')
-      .end(function (err, res) {
-        expect(err).to.be.null;
-        res.should.have.status(200);
-        res.should.be.html;
-        res.text.should.match(/User list/);
+        .get('/users')
+        .end(function (err, res) {
+          expect(err).to.be.null;
+          res.should.have.status(200);
+          res.should.be.html;
+          res.text.should.match(/User list/);
+          done();
+        });
+  });
+});
+
+describe('DELETE', function() {
+  it('should return error for non-existent user id', function(done) {
+    request
+      .delete('/users/non-existent-user-id')
+      .end(function(err, res) {
+        res.should.have.status(404);
         done();
       });
   });
