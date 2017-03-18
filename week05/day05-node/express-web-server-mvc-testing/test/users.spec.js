5ef8e448b16b44dbd21232455b484bc7628f4d21
Mohammed Yafii <myafii@spartaglobal.co>
2017-03-18 07:29:49 +0000

    add test to delete an existing user 2nd attempt

--- week05/day05-node/express-web-server-mvc-testing/test/users.spec.js
+++ week05/day05-node/express-web-server-mvc-testing/test/users.spec.js
@@ -9,24 +9,33 @@ var request;
 chai.should();
 chai.use(chaiHttp);
 
-describe('Users', function() {
+// We are looking for HTML that looks like this:
+// <a href="/users/58cbb8e616f8b0228f71b315">
+// We can the extract the user ID from the `href` attribute using a regex.
+function getFirstUserIdFromUserListHTML(html) {
+  var regEx = /\/users\/[0-9a-f]+/;
+  var result = regEx.exec(html)[0];
+  var pathElements = result.split('/');
+
+  return pathElements[2];
+}
+
+describe('Users', function () {
   beforeEach(function () {
     request = chai.request(app);
   });
-});
 
-describe('GET', function() {
-
-  it('should return error for invalid URL GET', function (done) {
-    request
+  describe('GET', function () {
+    it('should return error for invalid URL GET', function (done) {
+      request
         .get('/invalid_url')
         .end(function (err) {
           expect(err).not.to.be.null;
           done();
         });
-  });
-  it('should list all users for GET /users', function (done) {
-    request
+    });
+    it('should list all users for GET /users', function (done) {
+      request
         .get('/users')
         .end(function (err, res) {
           expect(err).to.be.null;
@@ -35,16 +44,31 @@ describe('GET', function() {
           res.text.should.match(/User list/);
           done();
         });
+    });
   });
-});
 
-describe('DELETE', function() {
-  it('should return error for non-existent user id', function(done) {
-    request
-      .delete('/users/non-existent-user-id')
-      .end(function(err, res) {
-        res.should.have.status(404);
-        done();
-      });
+  describe('DELETE', function () {
+    it('should return error for non-existent user id', function (done) {
+      request
+        .delete('/users/non-existent-user-id')
+        .end(function (err, res) {
+          res.should.have.status(404);
+          done();
+        });
+    });
+    it('should return correct result for existing user', function (done) {
+      request
+        .get('/users')
+        .end(function (err, res) {
+          var userId = getFirstUserIdFromUserListHTML(res.text);
+
+          request
+            .delete('/users/' + userId)
+            .end(function (err, res) {
+              res.should.have.status(200);
+              done();
+            });
+        });
+    });
   });
 });
