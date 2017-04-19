1098a431873efee726196182f080b92fddd68fef
Mohammed Yafii <myafii@spartaglobal.co>
2017-04-19 15:40:22 +0100

    Add test for add() method on TodosController

--- week10/day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
+++ week10/day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
@@ -1,10 +1,16 @@
 describe('TodosController', () => {
   let controllerToTest;
+  let MockTodosFactory;
 
   beforeEach(() => {
+    MockTodosFactory = {
+      list: [],
+      add: jasmine.createSpy(),
+      clear: jasmine.createSpy()
+    };
     module('todosApp');
     inject(($controller) => {
-      controllerToTest = $controller('TodosController');
+      controllerToTest = $controller('TodosController', { TodosFactory: MockTodosFactory });
     });
   });
 
@@ -16,4 +22,22 @@ describe('TodosController', () => {
       expect(controllerToTest.inputText).toEqual('');
     });
   });
+
+  describe('add()', () => {
+    it('should call TodosFactory.add() with correct parameter', () => {
+      const inputText = 'new todo 1';
+
+      controllerToTest.inputText = inputText;
+      controllerToTest.add();
+      expect(MockTodosFactory.add).toHaveBeenCalledWith(inputText);
+    });
+    it('should clear inputText', () => {
+      const inputText = 'new todo 2';
+
+      controllerToTest.inputText = inputText;
+      controllerToTest.add();
+      expect(controllerToTest.inputText).toEqual('');
+    });
+  });
+
 });
