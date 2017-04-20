b4b29318fc8496069ff6bf90e7add90902ddad4d
Mohammed Yafii <myafii@spartaglobal.co>
2017-04-20 11:47:29 +0100

    ES6-ify the front end

--- week10/day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/js/controllers/duck.controller.js
+++ week10/day04-angular-testing-with-services/angular-crud-with-ui-tests/frontend/js/controllers/duck.controller.js
@@ -1,53 +1,53 @@
 function DuckController($state, $stateParams, DuckFactory) {
-  var controller = this;
+  const controller = this;
 
-  controller.getDuck = function () {
-    var duckId = $stateParams.duckId;
+  controller.getDuck = () => {
+    const duckId = $stateParams.duckId;
 
     DuckFactory.getOne(duckId).then(
-      function success(response) {
+      (response) => {
         controller.selectedDuck = response.data;
       },
-      function error(error) {
+      (error) => {
         console.warn('Error getting duck:', error);
       }
     );
   };
 
-  controller.addDuck = function () {
+  controller.addDuck = () => {
     console.log('addDuck()');
     DuckFactory.createOne(controller.newDuck).then(
-      function success(response) {
+      (response) => {
         console.log('Created new duck:', response);
         $state.go('home');
       },
-      function error(error) {
+      (error) => {
         console.warn('Error creating duck:', error);
       }
     );
   };
 
-  controller.deleteDuck = function (duckId) {
+  controller.deleteDuck = (duckId) => {
     DuckFactory.deleteOne(duckId).then(
-      function success(response) {
+      (response) => {
         console.log('duck deleted:', response);
       },
-      function error(error) {
+      (error) => {
         console.warn('Error deleting duck:', error);
       }
     );
   };
 
-  controller.editDuck = function (duckId) {
-    $state.go('edit', { duckId: duckId });
+  controller.editDuck = (duckId) => {
+    $state.go('edit', { duckId });
   };
 
-  controller.updateDuck = function () {
+  controller.updateDuck = () => {
     DuckFactory.editOne(controller.selectedDuck.duck).then(
-      function success(response) {
+      (response) => {
         console.log('duck updated:', response);
       },
-      function error(error) {
+      (error) => {
         console.warn('Error updating duck:', error);
       }
     );
@@ -60,10 +60,10 @@ function DuckController($state, $stateParams, DuckFactory) {
     controller.newDuck = {};
     controller.colors = ['red', 'green', 'blue'];
     DuckFactory.getAll().then(
-      function success(response) {
+      (response) => {
         controller.allDucks = response.data;
       },
-      function error(error) {
+      (error) => {
         console.warn('Error getting ducks:', error);
       }
     );
